7c8ed433061f7901976cb803cadd6b00e16351bf
averak <abe12@mccc.jp>
2022-07-14 10:42:23 +0900

    [feat] 残差標準偏差と推定残差標準偏差のヒートマップを作成

--- src/usecase/estimate_usecase.py
+++ src/usecase/estimate_usecase.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from src.nnet.base_nnet import BaseNNet
+from src.property.human_property import HumanProperty
 from src.property.path_property import PathProperty
 from src.repository.human_repository import HumanRepository
 
@@ -14,6 +15,11 @@ class EstimateUseCase:
     path_property: PathProperty = PathProperty()
     """
     PATHプロパティ
+    """
+
+    human_property: HumanProperty = HumanProperty()
+    """
+    人間プロパティ
     """
 
     nnet: BaseNNet
@@ -74,23 +80,25 @@ class EstimateUseCase:
         figure = plt.figure()
         ax = figure.add_subplot(111)
         ax.set_aspect("equal", adjustable="box")
-        plt.hist2d(θ_pred_list_train, θ_true_list_train, bins=90, range=[(0, 90), (0, 90)], cmin=1)
+        plt.hist2d(θ_pred_list_train, θ_true_list_train, bins=self.human_property.max_age,
+                   range=[(0, self.human_property.max_age), (0, self.human_property.max_age)], cmin=1)
         plt.colorbar()
         plt.xlabel("θ")
         plt.ylabel("Age")
-        plt.xlim(0, 90)
-        plt.ylim(0, 90)
+        plt.xlim(0, self.human_property.max_age)
+        plt.ylim(0, self.human_property.max_age)
         plt.savefig(f"{self.path_property.heatmap_path}/θ_train.png")
 
         figure = plt.figure()
         ax = figure.add_subplot(111)
         ax.set_aspect("equal", adjustable="box")
-        plt.hist2d(θ_pred_list_test, θ_true_list_test, bins=90, range=[(0, 90), (0, 90)], cmin=1)
+        plt.hist2d(θ_pred_list_test, θ_true_list_test, bins=self.human_property.max_age,
+                   range=[(0, self.human_property.max_age), (0, self.human_property.max_age)], cmin=1)
         plt.colorbar()
         plt.xlabel("θ")
         plt.ylabel("Age")
-        plt.xlim(0, 90)
-        plt.ylim(0, 90)
+        plt.xlim(0, self.human_property.max_age)
+        plt.ylim(0, self.human_property.max_age)
         plt.savefig(f"{self.path_property.heatmap_path}/θ_test.png")
 
         # 残差標準偏差σのヒートマップを作成
@@ -115,3 +123,56 @@ class EstimateUseCase:
         plt.xlim(0, 50)
         plt.ylim(0, 50)
         plt.savefig(f"{self.path_property.heatmap_path}/σ_test.png")
+
+        # 残差標準偏差のヒートマップを作成
+        σ_pred_standard_deviation_list_train: list[float] = list(range(self.human_property.max_age))
+        standard_deviation_list_train: list[float] = list(np.zeros(self.human_property.max_age))
+        σ_pred_standard_deviation_list_test: list[float] = list(range(self.human_property.max_age))
+        standard_deviation_list_test: list[float] = list(np.zeros(self.human_property.max_age))
+
+        for i in range(self.human_property.max_age):
+            sum_train: float = 0.0
+            cnt_train: int = 0
+            sum_test: float = 0.0
+            cnt_test: int = 0
+
+            for j, σ in enumerate(σ_pred_list_train):
+                if int(σ) == i:
+                    sum_train += σ_true_list_train[j] ** 2
+                    cnt_train += 1
+
+            for j, σ in enumerate(σ_pred_list_test):
+                if int(σ) == i:
+                    sum_test += σ_true_list_test[j] ** 2
+                    cnt_test += 1
+
+            if cnt_train != 0:
+                standard_deviation_list_train[i] = np.sqrt(sum_train / cnt_train)
+            if cnt_test != 0:
+                standard_deviation_list_test[i] = np.sqrt(sum_test / cnt_test)
+
+        figure = plt.figure()
+        ax = figure.add_subplot(111)
+        ax.set_aspect('equal', adjustable='box')
+        plt.hist2d(σ_pred_standard_deviation_list_train, standard_deviation_list_train,
+                   bins=self.human_property.max_age,
+                   range=[(0, self.human_property.max_age), (0, self.human_property.max_age)], cmin=1)
+        plt.colorbar()
+        plt.xlabel("σ")
+        plt.ylabel("standard deviation")
+        plt.xlim(0, self.human_property.max_age)
+        plt.ylim(0, self.human_property.max_age)
+        plt.savefig(f"{self.path_property.heatmap_path}/σ_standard_deviation_train.png")
+
+        figure = plt.figure()
+        ax = figure.add_subplot(111)
+        ax.set_aspect('equal', adjustable='box')
+        plt.hist2d(σ_pred_standard_deviation_list_test, standard_deviation_list_test,
+                   bins=self.human_property.max_age,
+                   range=[(0, self.human_property.max_age), (0, self.human_property.max_age)], cmin=1)
+        plt.colorbar()
+        plt.xlabel("σ")
+        plt.ylabel("standard deviation")
+        plt.xlim(0, self.human_property.max_age)
+        plt.ylim(0, self.human_property.max_age)
+        plt.savefig(f"{self.path_property.heatmap_path}/σ_standard_deviation_test.png")
